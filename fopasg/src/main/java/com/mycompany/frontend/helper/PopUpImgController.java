b68991637f128924e07743d9a3c2d22ec507f7c0
ooijiahui <126597154+ooijiahui@users.noreply.github.com>
2024-12-26 00:16:37 +0800

    minor issue

--- fopasg/src/main/java/com/mycompany/frontend/helper/PopUpImgController.java
+++ fopasg/src/main/java/com/mycompany/frontend/helper/PopUpImgController.java
@@ -3,40 +3,47 @@ package com.mycompany.frontend.helper;
 import javafx.fxml.FXML;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class PopUpImgController {
 
-    /*** ELEMENTS WITH FX:ID.  
+    /***
+     * ELEMENTS WITH FX:ID.
      * 
-     * ***/
+     ***/
+    @FXML
+    private Pane imgContainer;
+
     @FXML
     private ImageView img;
-    
+
     @FXML
     private ImageView crossIcon;
 
-
-    /*** INITILIZATION OF THE CONTROLLER.
+    /***
+     * INITILIZATION OF THE CONTROLLER.
      * 
-     * ***/
+     ***/
     public void initialize() {
         // Add a click event to the cross icon
         crossIcon.setOnMouseClicked(event -> closeMessageWindow());
     }
 
-    /*** METHOD TO CLOSE THE IMAGE WINDOW.
+    /***
+     * METHOD TO CLOSE THE IMAGE WINDOW.
      * 
-     * ***/
+     ***/
     private void closeMessageWindow() {
         Stage stage = (Stage) crossIcon.getScene().getWindow();
         stage.close();
     }
 
-    /*** METHOD TO SET THE IMAGE.
+    /***
+     * METHOD TO SET THE IMAGE.
      * 
-     * ***/
+     ***/
     public void setImage(Image img) {
-        this.img.setImage(img); 
+        this.img.setImage(img);
     }
 }
