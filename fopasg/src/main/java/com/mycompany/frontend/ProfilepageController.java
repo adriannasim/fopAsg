c260899ded7554491f42a59b7f4d9bc3519f1799
ooijiahui <126597154+ooijiahui@users.noreply.github.com>
2024-12-25 23:56:45 +0800

    Added password strength check effect for profile

--- fopasg/src/main/java/com/mycompany/frontend/ProfilepageController.java
+++ fopasg/src/main/java/com/mycompany/frontend/ProfilepageController.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
- */
 package com.mycompany.frontend;
 
 import java.io.IOException;
@@ -11,12 +7,13 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
 
-/**
- * FXML Controller class
- *
- * @author User
- */
+/***
+ * THIS CONTROLLER CLASS IS USED FOR profile-page.fxml
+ * 
+ ***/
+
 public class ProfilePageController extends SharedPaneCharacteristics {
     
     /*** ELEMENTS WITH FX:ID  
@@ -41,7 +38,11 @@ public class ProfilePageController extends SharedPaneCharacteristics {
     private ImageView passwordEdit;
 
     @FXML
+    private Text passwordMsg;
+
+    @FXML
     private Button submitBtn;
+
 
     /*** INITILIZATION OF THE CONTROLLER
      * 
@@ -80,16 +81,37 @@ public class ProfilePageController extends SharedPaneCharacteristics {
             password.requestFocus();
         });
 
+        // Check for password strength
+        password.setOnKeyTyped(e->{
+            // Weak password
+            if (password.getText().length() < 6){ // NEED TO CHANGE THE FORMAT COMPARE
+                password.setStyle("-fx-background-color: #FF9696;");
+                passwordMsg.setText("Password strength weak.");
+                passwordMsg.setStyle("-fx-fill: #FF9696;");
+            }
+            // Moderate password 
+            else if (password.getText().equals("123456")){ // NEED TO CHANGE THE FORMAT COMPARE
+                password.setStyle("-fx-background-color: #669A9D;");
+                passwordMsg.setText("Password strength moderate.");
+                passwordMsg.setStyle("-fx-fill: #669A9D;");
+            }
+            // Strong password 
+            else if (password.getText().equals("abc123")){ // NEED TO CHANGE THE FORMAT COMPARE
+                password.setStyle("-fx-background-color: #9ABF80;");
+                passwordMsg.setText("Password strength strong.");
+                passwordMsg.setStyle("-fx-fill: #9ABF80;");
+            }
+        });
+
+        // When user click on submit button
         submitBtn.setOnMouseClicked(e->{
             try{
+                // Pop up a confimation message
                 App.openConfirmationPopUp("Do you confirm to change your details?", "Your changes has been saved.", "Failed to save the details.");
             } catch (IOException ex){
                 ex.printStackTrace();
             }
-            
         });
 
-    }
-    
-    
+    } 
 }
